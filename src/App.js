482742ad28787748ab51a8de22bbbf1762fc8931
shawnbarbel <barbels33@gmail.com>
2021-02-25 15:09:23 -0500

    main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./app.css"
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-black text-3xl">Hello World</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { HotModuleReplacementPlugin } = require('webpack')
+
+module.exports = (env, argv) => {
+  return {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].bundle.css",
+        chunkFilename: "[id].css",
+      }),
+      new HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+      open: true,
+      clientLogLevel: 'silent',
+      contentBase: './build',
+      historyApiFallback: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          include: path.resolve(__dirname, "src"),
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  [
+                    "@babel/preset-env",
+                    {
+                      targets: {
+                        node: "12",
+                      },
+                    },
+                  ],
+                  "@babel/preset-react",
+                ],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, "src"),
+          exclude: /node_modules/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader", "postcss-loader",
+          ],
+        },
+      ],
+    },
+  };
+};
