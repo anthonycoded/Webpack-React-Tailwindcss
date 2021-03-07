53f7e7ddbed8ffd100b91ab5102e5ea5b9e8b7b8
anthonycoded <barbels33@gmail.com>
2021-03-07 13:45:29 -0500

    added hamburger svg

---
+++ src/components/UI/Menu.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          width: "35px",
+          height: "5px",
+          backgroundColor: "#ffab00",
+          margin: "6px 0",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "35px",
+          height: "5px",
+          backgroundColor: "#ffab00",
+          margin: "6px 0",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "35px",
+          height: "5px",
+          backgroundColor: "#ffab00",
+          margin: "6px 0",
+        }}
+      ></div>
+    </React.Fragment>
+  );
+};
+
+export default Menu;
