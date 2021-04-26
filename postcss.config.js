b997659bb2d105ea29b7b4e25a8184cb2cf58607
shawn barbel <barbels33@gmail.com>
2021-04-26 10:23:32 -0400

    PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,27 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require("tailwindcss");
+const purgeCSS = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    purgeCSS({
+      content: ["./src/**/*.js"],
+      css: ["./src/**/*.css"],
+      safelist: [
+        "html",
+        "body",
+        "img",
+        "button",
+        "padding",
+        "margin",
+        "width",
+        "xl",
+        "sm",
+        "md",
+        "lg",
+        "xl",
+      ],
+    }),
   ],
-
-}
+};
