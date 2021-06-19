89566ded47a8fafe8b6f10a853dc7d9fcd877dce
shawn barbel <barbels33@gmail.com>
2021-06-18 22:51:12 -0400

    added scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./app.css";
 import Admin from "./components/Admin";
 import Header from "./components/UI/Header";
-import Footer from "./components/UI/Footer"
+import Footer from "./components/UI/Footer";
 import LandingPage from "./components/LandingPage";
 import Login from "./components/Authentication/Login";
 import Register from "./components/Authentication/Register";
-
+import ScrollIntoView from "./components/UI/ScrollIntoView";
 
 const App = () => {
   return (
     <div className="h-full">
       <Router>
         <Header className="display-block"></Header>
-        <Switch>
-          <Route path="/" exact>
-            <LandingPage></LandingPage>
-          </Route>
-          <Route path="/admin" exact component={Admin}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/register" component={Register}></Route>
-        </Switch>
+        <ScrollIntoView>
+          <Switch>
+            <Route path="/" exact>
+              <LandingPage></LandingPage>
+            </Route>
+            <Route path="/admin" exact component={Admin}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/register" component={Register}></Route>
+          </Switch>
+        </ScrollIntoView>
         <Footer></Footer>
       </Router>
     </div>
