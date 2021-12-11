c165ff0eb0c1193f274d71c3ff9daf60c2e0f9c0
shawn barbel <barbels33@gmail.com>
2021-12-10 19:11:42 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        primary: {
+          main: "#0EA44B",
+          light: "rgba(14, 164, 75, 0.4)",
+        },
+        secondary: "",
+        active: "#5FDBA7",
+        disabled: "#D3D3D3",
+        inActive: "#bdbdbd",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
